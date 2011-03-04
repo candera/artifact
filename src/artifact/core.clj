1bb1febd95337370116b96ea4bbba920fe03f513
Craig Andera <candera@wangdera.com>
2011-03-03 20:40:27 -0500

    Added empty project

---
+++ src/artifact/core.clj
@@ -0,0 +1,1 @@
+(ns artifact.core)
---
+++ test/artifact/test/core.clj
@@ -0,0 +1,6 @@
+(ns artifact.test.core
+  (:use [artifact.core] :reload)
+  (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
