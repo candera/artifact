787075b825e15d0cbc4c7c54126b7da2a48552ae
Craig Andera <candera@wangdera.com>
2011-03-05 07:34:13 -0500

    Added beginnings of join processing.

--- src/artifact/core.clj
+++ src/artifact/core.clj
@@ -1,5 +1,6 @@
 (ns artifact.core
   (:use [ring.adapter.jetty :only (run-jetty)]
+	[ring.util.response :only (response)]
 	[compojure.core]
 	[clojure.contrib.prxml :only (prxml *prxml-indent* *html-compatible*)])
   (:require [compojure.route :as route]
@@ -19,13 +20,34 @@
     [:title "Artifact (Pre-Alpha)"]]
    [:body
     [:p "Welcome to artifact! Actual functionality still under development."]
-    [:form {:action "/api/join" :method "post"}
+    [:p "Join a game by entering your name here."]
+    [:form {:action "/game" :method "post"}
      "Name:"
      [:input {:type "text" :name "name"}]
      [:input {:type "submit" :value "Join"}]]]])
 
+(defn- request-dump [req]
+  [:div {:class "request-dump"}
+   [:p "Params :"]
+   [:table
+    [:tr [:th "Name"] [:th "Value"]]
+    (map (fn [[k v]] [:tr [:td (str k)] [:td (str v)]]) (:params req))]
+   [:p "Raw: " (str (:params req))]])
+
+(defn- game-page [req]
+  (response
+   (to-html-str
+    [:html
+     [:head
+      [:title "Artifact (Pre-Alpha)"]
+      [:body
+       [:p "You have joined, "
+	(get (:params req) :name "<No name>")]
+       (request-dump req)]]])))
+
 (defroutes main-routes
   (GET "/" [] (to-html-str index))
+  (POST "/game" [] game-page)
   (route/resources "/")
   (route/not-found "Page not found"))
 
