9983478f917dd3acbf57010747ab2755a5b072d3
Craig Andera <candera@wangdera.com>
2011-03-08 06:41:04 -0500

    Upgraded to latest stable versions of ring and compojure.

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write"
   :dependencies [[org.clojure/clojure "1.2.0"]
                  [org.clojure/clojure-contrib "1.2.0"]
-		 [ring/ring-jetty-adapter "0.3.6"]
-		 [compojure "0.6.0"]]
+		 [ring/ring-jetty-adapter "0.3.7"]
+		 [compojure "0.6.1"]]
   :dev-dependencies [[swank-clojure "1.2.0"]]
   :main artifact.core)
