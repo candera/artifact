7d571b28472922cf241411f2e1cbeed01e205d56
Craig Andera <candera@wangdera.com>
2011-03-11 07:39:14 -0500

    Now parsing returned JSON game state.

--- resources/public/script/game.js
+++ resources/public/script/game.js
@@ -1,8 +1,14 @@
+var gameState = [];
+
+function mergeGameState (data, status, jqXHR) {
+    var str = "";
+    for (var i = 0; i < data.length; ++i)
+    {
+	str += data[i] + "\n";
+    }
+    document.getElementById("gameState").innerText = str;
+}
+
 function updateGameState () {
-    $.get(
-	gameStateUrl,
-	{},
-	function (data) {
-	    document.getElementById("gameState").innerText = data;
-	});
+    $.getJSON(gameStateUrl, {}, mergeGameState);
 }
