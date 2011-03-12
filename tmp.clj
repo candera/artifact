3d5426ab2cc9b2eaca9fb05bd8e43bb968f70151
Craig Andera <candera@wangdera.com>
2011-03-12 07:34:50 -0500

    Updated scratch file.

--- tmp.clj
+++ tmp.clj
@@ -25,3 +25,54 @@
 (use 'clojure.pprint)
 
 (pprint @#'artifact.triplestore/triplestore)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(in-ns 'artifact.game)
+(def store (create-triplestore))
+(initialize-game store)
+
+(defn priority [& triples]
+  (first (filter (fn [[[_ a] _]] (= a "priority")) triples)))
+
+(->> (query store ["acl:*" "*" "*"])
+     (group-by entity)
+     (map second)
+     (map priority)
+     (pprint))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(def ^{:private true :doc "Defines the visibility rules for the game"}
+  acl-rules
+  [["*"        "*"     :public]
+   ["player:*" "*"     :private]
+   ["player:*" "ready" :public]
+   ["player:*" "name"  :public]])
+
+(def store (create-triplestore))
+
+(add-triples store
+	     ["foo" "bar" 1]
+	     ["player:1" "ready" true]
+	     ["player:1" "token" "abcd"]
+	     ["player:1" "name" "Craig"]
+	     ["player:2" "ready" false])
+
+(use 'artifact.triplestore)
+
+(defn visibility [triple rule]
+  (let [[espec aspec vis] rule
+	pred (build-spec-filter [espec aspec "*"])]
+    (when (pred triple) vis)))
+
+(defn visibilities [triple]
+  (map #(visibility triple %) acl-rules))
+
+(->> (get-all-triples store)
+     ;; Something that turns each triple into [:public :private nil :public]
+     (map visibilities)
+     (map  #(last (filter identity %)))
+     (pprint))
+
+(pprint (get-all-triples store))
+(pprint  (map #(is-visible? #{"global"} %) (get-all-triples store)))
