be53ff336f454e51b3a9a6c9eccd99336056cf26
Craig Andera <candera@wangdera.com>
2011-03-15 07:46:58 -0400

    Added diff support to game state processing.

--- resources/public/script/game.js
+++ resources/public/script/game.js
@@ -2,26 +2,78 @@ var gameState = [];
 
 $(document).everyTime(500, updateGameState);
 
-function displayGameState() {
+function displayGameState(state) {
     var str = "";
     for (var i = 0; i < gameState.length; ++i)
     {
 	str += gameState[i] + "\n";
     }
-    document.getElementById("gameState").innerText = str;
+    $("#gameState").text(str);
 }
 
-function mergeGameState (data, status, jqXHR) {
-    gameState = data;
-    displayGameState(); 
+function getTripleValue(store, entity, att){
+    for (var i = 0; i < store.length; ++i) {
+	var triple = store[i];
+	if (triple[0] == entity && triple[1] == att) {
+	    return triple[2];
+	}
+    }
+    return null; 
+}
+
+Array.prototype.diff = function(a) {
+    return this.filter(function(i) {return !(a.indexOf(i) > -1);});
+};
+
+function diff(before, after) {
+    var diff = new Object();
+
+    if (before == null && after == null) {
+	diff.additions = [];
+	diff.deletions = [];
+    }
+    else if (before == null) {
+	diff.additions = after;
+	diff.deletions = [];
+    }
+    else if (after == null)
+    {
+	diff.additions = [];
+	diff.deletions = before;
+    }
+    else {
+	diff.additions = after.diff(before);
+	diff.deletions = before.diff(after);
+    }
+
+    return diff;
+}
+
+function mergeGameState (newState, status, jqXHR) {
+    displayGameState(newState); 
     
-    // switch (getTripleValue("game", "phase")) {
-    // // case "joining": 
-    // // 	$('#joining-ui').show(250, 'slow');
-    // // 	break;
-    // // default:
-    // // 	$('#joining-ui').hide(250, 'slow');
-    // }
+    // Iterate over the list of players in game,players and display a
+    // row for each, highlighting if it's us
+    var newPlayers = getTripleValue(newState, "game", "players"); 
+    var oldPlayers = getTripleValue(gameState, "game", "players");
+
+    var changes = diff(oldPlayers, newPlayers);
+
+    var additions = changes.additions;
+    var deletions = changes.deletions;
+
+    for (var i = 0; i < additions.length; ++i) {
+	var addition = additions[i];
+
+	// $("#joined-players").find("tr td :contains" + 
+	// 			    getTripleValue(newState, player, "name") +
+	// 			    "</td><td>" + 
+	// 			    getTripleValue(newState, player, "ready") ? 
+	// 			    "Ready" : "Not Ready" + 
+	// 			    "</td></tr>"); 
+    }
+
+    gameState = newState;
 }
 
 function updateGameState () {
--- src/artifact/ui.clj
+++ src/artifact/ui.clj
@@ -67,10 +67,7 @@ game."
 	  [:raw! (str "var gameStateUrl='" (state-url token) "';")]]
 	 [:script {:src "/script/game.js"} ""]]
 	[:body
-	 [:p "The following players have joined:"
-	  (or player-name "<No name>")]
 	 [:div {:id "joining-ui"}
-	  "UI for joining up will be shown here."
 	  [:table {:id "joined-players"}]]
 	 [:textarea {:id "gameState" :readonly "readonly" :rows 20}
 	  "game state will go here"]]])))))
