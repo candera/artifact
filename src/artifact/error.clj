0701fa3cfe72a23de659e38678e7db06427823b2
Craig Andera <candera@wangdera.com>
2011-05-06 22:40:51 -0400

    Furthered work on error handling macros. Still WIP.

--- src/artifact/error.clj
+++ src/artifact/error.clj
@@ -5,16 +5,46 @@
 (defprotocol ApplicationError
   (data [this]))
 
-(defn error [d]
+(defn app-throw
+  "Throws an error that can be handled via an app-try statments. Any
+  type of data can be thrown: it need not derive from Throwable."
+  [d]
   (throw (proxy [Throwable artifact.error.ApplicationError] []
            (data [] d))))
 
-;; TODO: You can't expand just a catch: try is handled specially by
-;; the compiler, and macroexpansions inside the try block don't work
-;; right. So this macro needs to change to produce both the try and
-;; the catch.
-(defmacro error-catch [name & body]
-  `(catch Throwable t#
-     (if (satisfies? ApplicationError t#)
-       (let [~name (data t#)] ~@body)
-       (throw t#))))
+(defn- classify-clause
+  "Classifies a clause in an app-try statement according to how it
+  needs to be emitted into the macro expansion."
+  [clause]
+  (case (first clause)
+        'app-catch :app-catch
+        'catch :catch
+        :other))
+
+(defmacro app-try
+  "Expands into a try-catch block that catch both normal Java
+  exceptions and the type of error thrown by app-try.
+
+  (app-try
+     (normal-statement)
+     (normal-statement)
+     (when true (app-throw :whatever))
+     (app-catch d
+        (println d))  ; prints :whatever
+     (catch Exception e
+        (println \"Some other exception was thrown\")))
+
+  Note that as currently implemented, you cannot catch Throwable, as
+  the macro expands to include a clause that does that. If you need to
+  catch Throwable, wrap the app-try in a regular try."
+  [& body]
+  (let [[statements [[_ name & handlers]] standard-catches] (partition-by classify-clause body)]
+    `(try
+       ~@statements
+       ~@standard-catches
+       (catch Throwable t#
+         (if (satisfies? ApplicationError t#)
+           (let [~name (data t#)] ~@handlers)
+           (throw t#))))))
+
+
