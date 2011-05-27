47ee21313f1113cfc5616079f1896403cae8854a
Craig Andera <candera@wangdera.com>
2011-05-27 14:15:11 -0700

    throws macro now allows app-throw comparisons.

--- test/artifact/test/common.clj
+++ test/artifact/test/common.clj
@@ -1,5 +1,17 @@
 (ns artifact.test.common
-  (:use [clojure.test]))
+  (:use [clojure.test]
+        [artifact.error]))
 
-(defmacro throws [x & body]
-  `(is (try ~@body false (catch ~x ~'_ true))))
+(defmacro throws
+  "Emits a call to the is macro that will pass if the specified x is
+  'thrown' by the body expressions. x can be either a
+  java.lang.Throwable, in which case we look for a regular exception
+  of that type to be raised, or it can be anything else, in which case
+  we look for an app-throw that threw that value."
+  [x & body]
+  (if (and (symbol? x) (isa? (resolve x) java.lang.Throwable))
+    `(is (try ~@body false (catch ~x ~'_ true)))
+    `(is (app-try
+          ~@body
+          false
+          (~'app-catch e# (= e# ~x))))))
