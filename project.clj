fcc1aa33d118c5f8c9c0dd73010837e342518fe8
Craig Andera <candera@wangdera.com>
2012-05-04 16:49:03 -0400

    Upgrade/add deps: Clojure, Marginalia, Datomic

--- project.clj
+++ project.clj
@@ -2,22 +2,22 @@
   :description "The server side of the game Artifact, an archaeology-themed board
   game. To run it, fire up the server and connect to it with a
   browser."
-  :dependencies [[org.clojure/clojure "1.3.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [ring/ring-jetty-adapter "1.0.1"]
                  [ring/ring-core "1.0.1"]
-                 [ring "1.0.1"]         ; TODO: Do I need this and the
-                                        ; other Ring libraries?
+                 ;; TODO: Do I need this and the other Ring libraries?
+                 [ring "1.0.1"]
                  [org.clojure/data.json "0.1.1"]
                  [compojure "1.0.0"]
                  [enlive "1.0.0"]
                  [org.clojure/clojurescript "0.0-927"]
+                 [com.datomic/datomic "0.1.3065"]
                  ;; This next one is needed to prevent problems with
                  ;; lein-swank. See
                  ;; https://github.com/technomancy/swank-clojure
                  [clj-stacktrace "0.2.4"]]
-  :dev-dependencies [[jline "0.9.94"]
-                     [marginalia "0.7.0-SNAPSHOT"]
-                     [lein-marginalia "0.7.0-SNAPSHOT"]]
+  :plugins [[marginalia "0.7.0"]
+            [lein-marginalia "0.7.0"]]
   :git-dependencies [["https://github.com/brentonashworth/one.git"
                       "M002"]
                      ["https://github.com/levand/domina.git"
